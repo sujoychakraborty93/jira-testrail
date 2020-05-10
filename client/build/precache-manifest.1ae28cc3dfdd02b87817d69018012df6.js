79ae79d9194e76ae008e857a7a1aa84e390b2237
sujoychakraborty93 <sujoychakraborty93@gmail.com>
2020-05-10 14:17:12 -0700

    staging changes made to build/ folder inside client react code

---
+++ client/build/precache-manifest.1ae28cc3dfdd02b87817d69018012df6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acfa8fd7af12ce66350f982ca3a9edbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "efc2887341f7fcd41d91",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "e6fdb0e1bca95a212a27",
+    "url": "/static/css/main.ec091fdc.chunk.css"
+  },
+  {
+    "revision": "efc2887341f7fcd41d91",
+    "url": "/static/js/2.4ee47054.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.4ee47054.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6fdb0e1bca95a212a27",
+    "url": "/static/js/main.78f0de00.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
